be3170e8db75c28220585d3014e1c9cd16b06ee2
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-11-28 16:41:16 +0200

    save base64 to local file

--- server.go
+++ server.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 )
 
 type UploadImageDTO struct {
@@ -24,10 +26,40 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	err = saveToFile(file)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		fmt.Println(err)
+		return
+	}
+
 	result := map[string]string{"result": "success"}
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(result)
+}
+
+func saveToFile(input UploadImageDTO) error {
+	// Decode base64 string to []byte
+	decoded, err := base64.StdEncoding.DecodeString(input.File)
+	if err != nil {
+		return err
+	}
+
+	// Create file
+	file, err := os.Create("image.png")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Write data to file
+	_, err = file.Write(decoded)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 func main() {
