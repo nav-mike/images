a5c34eb07a5e9c5c431faae21ddc0cc350073bcc
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-11-28 16:27:37 +0200

    config response

--- server.go
+++ server.go
@@ -1,17 +1,33 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
+
+type UploadImageDTO struct {
+	File string
+}
 
 func UploadHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
 		return
 	}
 
-	fmt.Println(r.Body)
-	fmt.Fprint(w, "[POST] UploadHandler")
+	var file UploadImageDTO
+
+	err := json.NewDecoder(r.Body).Decode(&file)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		fmt.Println(err)
+		return
+	}
+
+	result := map[string]string{"result": "success"}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(result)
 }
 
 func main() {
