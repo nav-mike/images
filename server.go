397a3ef0aa731f9b5ee44d0b210c2c60e2f257cf
Mikhail Navrotskii <m.navrotskiy@gmail.com>
2022-11-29 11:17:08 +0200

    add error for wrong method

--- server.go
+++ server.go
@@ -33,6 +33,7 @@ var imageSizes = [...]ImageSize{
 
 func UploadHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
